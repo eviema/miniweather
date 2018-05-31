e65ee0f1eb5abeac60cd2d34d21ff0522f63c457
Evie Ma <yfma89@gmail.com>
2018-05-31 23:18:13 +1000

    get weather data from api and show on index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,33 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+Page({
+  data: {
+    nowTemp: '14°',
+    nowWeather: '阴天'
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        })
+      }
+    })
+  }
+})
