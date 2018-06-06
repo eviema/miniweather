3c8069c5839eaf752a4fb6075f0af3410d752c20
Evie Ma <yfma89@gmail.com>
2018-06-06 15:23:22 +1000

    add date and temp range

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,9 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: '',
-    hourlyWeather: []
+    hourlyWeather: [],
+    todayTemp: '',
+    todayDate: '',
   },
   onPullDownRefresh() {
     this.getNow(()=> {
@@ -41,6 +43,7 @@ Page({
         let result = res.data.result
         this.setNow(result)
         this.setHourlyWeather(result)
+        this.setToday(result)
       },
       complete: () => {
         callback && callback()
@@ -75,5 +78,15 @@ Page({
     this.setData({
       hourlyWeather: hourlyWeather
     })
+  },
+  setToday(result){
+    let date = new Date()
+    this.setData({
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+      todayDate: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 今天`
+    })
+  },
+  onTapDayWeather(){
+    wx.showToast()
   }
 })
